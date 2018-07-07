190e01a123aab2e9f7831fa2edd49af0b9561a19
Mark Finn <mark@mfinn.net>
2018-07-07 21:15:31 +0000

    moving main

--- display/main.py
+++ display/main.py
@@ -1,39 +1,10 @@
 from __future__ import division
-##
- #  @filename   :   main.cpp
- #  @brief      :   4.2inch e-paper display (B) demo
- #  @author     :   Yehui from Waveshare
- #
- #  Copyright (C) Waveshare     August 15 2017
- #
- # Permission is hereby granted, free of charge, to any person obtaining a copy
- # of this software and associated documnetation files (the "Software"), to deal
- # in the Software without restriction, including without limitation the rights
- # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- # copies of the Software, and to permit persons to  whom the Software is
- # furished to do so, subject to the following conditions:
- #
- # The above copyright notice and this permission notice shall be included in
- # all copies or substantial portions of the Software.
- #
- # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- # FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- # LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- # THE SOFTWARE.
- ##
 
 import epd4in2b
 import Image
 import ImageFont
 import ImageDraw
 import ImageMath
-
-import sys
-sys.path.append('..')
-import thermocouple
 
 
 def textbox(draw, text, size, fill=128, center=None, corner=None, align='center'):
@@ -98,35 +69,8 @@ class Screen(object):
 
 
   def draw(self, draw):
-
-  #    draw_black.rectangle((0, 6, 400, 30), fill = 0)
-      temp = '%2.1f'%(t.read(True)*9.0/5.0+32)
-  #    temp='Hi\nAsron'
-      print temp
-      textbox(draw, temp, size=(200,100), center=(200,150), fill = 0)
-  #    draw_black.arc((40, 80, 180, 220), 0, 360, fill = 0)
-      # display the frames
-
-
-  def dxraw(self, draw, num=[
-#'88.2',
-'89.2',
-'87.3',
-#'88.2',
-'88.7']):
-      temp = num[0]
-      del num[0]
-      print temp
-      textbox(draw, temp, size=(200,100), center=(200,150), fill = 0)
-    
-  
-t = thermocouple.max6675(0,0)
-
-def main():
-    s=Screen((1,0))
-
-    while True:
-      s.refresh()
+      textbox(draw, "Hello, World!", size=(200,200), center=(200,150), fill = 0)
 
 if __name__ == '__main__':
-    main()
+    s=Screen((1,0))
+    s.refresh()
